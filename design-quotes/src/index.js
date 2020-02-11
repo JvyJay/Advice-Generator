4d1eeeaad02d18ec3db1701140c41d71676bd075
Jadon Guzman <jjkolet25@tutanota.com>
2020-02-10 23:57:03 -0800

    inital-commit

---
+++ design-quotes/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+
+reactDOM.render(<App />, document.getElementById('root'));
